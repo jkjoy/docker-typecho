bf1be0d5fa21240457f0e4d68c02adbf5b6791fa
浪子 <jkjoy@live.cn>
2024-11-25 18:26:57 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
+
 # 检查 /app/data 目录是否为空
 if [ -z "$(ls -A /app/data)" ]; then
-    echo "/app/data 目录为空，开始复制文件..."
+    echo "/app/data 目录为空"
     cp -r /app/usr/* /app/data/
     echo "文件复制完成。"
 else
-    echo "/app/data 目录不为空，跳过复制操作。"
+    echo "/app/data 目录不为空，跳过。"
 fi
-# Start PHP-FPM in the foreground
-php-fpm83 -F &
 
-# Start Nginx in the foreground
+# 设置 /app/data 目录的权限为可写
+chmod -R 777 /app/data
+
+# 创建符号链接
+ln -sfn /app/data /app/usr
+
+# 启动 PHP-FPM 和 Nginx
+php-fpm83 -D
 nginx -g "daemon off;"
